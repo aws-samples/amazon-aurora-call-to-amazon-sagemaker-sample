fa21607bafb11231945df70b45c8bebaef3e9965
root <root@ip-172-30-0-26.us-west-2.compute.internal>
2021-11-12 00:45:59 +0000

    b_11648

--- stk-sample/redshift/load.sh
+++ stk-sample/redshift/load.sh
@@ -16,5 +16,7 @@ do
   aws s3 cp $csv_file $s3_dest
   echo "aws s3 cp exit code="$?
   psql -A -t -c "copy orders_sorted from '$s3_dest' iam_role 'arn:aws:iam::584416962002:role/dms-access-for-endpoint' timeformat 'YYYY-MM-DD HH:MI:SS' maxerror as 250;"
-  echo "psql copy from s3 exit code="$?
+  echo "psql copy from s3 to orders_sorted exit code="$?
+  psql -A -t -c "copy orders from '$s3_dest' iam_role 'arn:aws:iam::584416962002:role/dms-access-for-endpoint' timeformat 'YYYY-MM-DD HH:MI:SS' maxerror as 250;"
+  echo "psql copy from s3 to orders exit code="$?
 done
--- stk-sample/redshift/vacuum.sh
+++ stk-sample/redshift/vacuum.sh
@@ -8,6 +8,8 @@ export PGUSER=$DB_USER
 while true
 do
   psql -A -e -t -c "vacuum sort only orders_sorted to 100 percent;"
-  echo "psql vacuum exit code="$?
+  echo "psql vacuum orders_sorted exit code="$?
+  psql -A -e -t -c "vacuum sort only orders to 100 percent;"
+  echo "psql vacuum orders exit code="$?
   sleep `awk -v min=10 -v max=20 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
 done
