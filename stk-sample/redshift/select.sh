8e6495f0a0874786d7bd9e056afdbb2c3c0d4955
root <root@ip-172-30-0-26.us-west-2.compute.internal>
2021-11-18 02:58:25 +0000

    b_21206

--- stk-sample/redshift/select.sh
+++ stk-sample/redshift/select.sh
@@ -7,11 +7,10 @@ export PGUSER=$DB_USER
 
 while true
 do
-  psql -A -e -t -c "select avg(bigint_null_10),sum(bigint_nonull_3) from orders_sorted where updated_at > getdate() - '30 hour'::INTERVAL and updated_at < getdate() - '9.9995 hour'::INTERVAL and bigint_nonull_1 > 9000 and bigint_null_7 < 10000000 group by updated_at;"
-  echo "psql select exit code="$?
-  psql -A -e -t -c "select avg(bigint_null_10),sum(bigint_nonull_3) from orders_sorted where bigint_nonull_1 > 9000 and bigint_null_7 < 10000000 group by updated_at;"
-  echo "psql select exit code="$?
-  psql -A -e -t -c "select count(*),text_notnull_1 from orders_sorted where updated_at > getdate() - '30 hour'::INTERVAL and updated_at < getdate() - '9.9995 hour'::INTERVAL and text_notnull_1 like '%3229e5d083f7%' group by text_notnull_1;"
-  echo "psql select cube exit code="$?
-  sleep `awk -v min=10 -v max=20 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
+  psql -A -e -t -c "select avg(bigint_null_10),sum(bigint_nonull_3) from orders_sorted where updated_at > getdate() - '30 hour'::INTERVAL and updated_at < getdate() - '9.9995 hour'::INTERVAL and idempotency_key<(select max(idempotency_key) from orders) and idempotency_key>(select max(idempotency_key)-(random() * 100000 + 1)::int from orders_sorted);"
+  echo "psql select orders_sorted  exit code="$?
+  psql -A -e -t -c "select avg(bigint_null_10),sum(bigint_nonull_3) from orders where updated_at > getdate() - '30 hour'::INTERVAL and updated_at < getdate() - '9.9995 hour'::INTERVAL and idempotency_key<(select max(idempotency_key) from orders) and idempotency_key>(select max(idempotency_key)-(random() * 100000 + 1)::int from orders);"
+  echo "psql select orders_sorted  exit code="$?
+  psql -A -e -t -c "select count(*),text_notnull_1 from orders where updated_at > getdate() - '30 hour'::INTERVAL and updated_at < getdate() - '9.9995 hour'::INTERVAL;"
+  echo "psql select orders exit code="$?
 done
--- stk-sample/redshift/update.sh
+++ stk-sample/redshift/update.sh
@@ -7,6 +7,7 @@ export PGUSER=$DB_USER
 
 while true
 do
-  psql -A -e -t -c "update orders set updated_at=getdate() where substring(text_notnull_1,1,10) like '%'||substring(md5(RANDOM()::TEXT),1,10)||'%';"
+  #psql -A -e -t -c "update orders set updated_at=getdate() where substring(text_notnull_1,1,10) like '%'||substring(md5(RANDOM()::TEXT),1,10)||'%';"
+  psql -A -e -t -c "update orders set updated_at=getdate() where idempotency_key<(select max(idempotency_key) from orders) and idempotency_key>(select max(idempotency_key)-(random() * 100000 + 1)::int from orders);"
   echo "psql update exit code="$?
 done
