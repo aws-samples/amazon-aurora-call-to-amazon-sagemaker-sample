4978fbcc0ba0916cd4dd08ad645421416ee9fe82
root <root@ip-172-30-0-26.us-west-2.compute.internal>
2021-11-18 05:11:19 +0000

    b_17623

--- stk-sample/redshift/update.sh
+++ stk-sample/redshift/update.sh
@@ -7,6 +7,6 @@ export PGUSER=$DB_USER
 
 while true
 do
-  psql -A -e -t -c "update orders set updated_at=getdate() where idempotency_key<(select max(idempotency_key)-10000000 from orders) and idempotency_key>(select max(idempotency_key)-(random() * 100000 + 1)::int-10000000 from orders);"
+  psql -A -e -t -c "update orders set updated_at=getdate()-(random() * 3 + 1)::int,TEXT_NOTNULL_1=MD5(random()::text)||MD5(random()::text)||MD5(random()::text)||MD5(random()::text) WHERE IDEMPOTENCY_KEY<(SELECT MAX(IDEMPOTENCY_KEY)-10000000 FROM ORDERS) AND IDEMPOTENCY_KEY>(SELECT max(idempotency_key)-(random() * 100000 + 1)::int-10000000 from orders);"
   echo "psql update exit code="$?
 done
