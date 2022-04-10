776b7a7c347e4b88b6e1bac2dd0392e673ef4c25
Biran <birayaha@3c063041fedb.ant.amazon.com>
2022-04-09 17:04:16 -0700

    inference pipeline

--- build-scripts/app.py
+++ build-scripts/app.py
@@ -18,14 +18,24 @@ class SMStack(Stack):
     model = sagemaker.CfnModel(self, "aurora-ml",
       execution_role_arn="arn:aws:iam::212076617619:role/service-role/AmazonSageMaker-ExecutionRole-20220331T142126",
       model_name="aurora-ml",
-
       containers=[sagemaker.CfnModel.ContainerDefinitionProperty(
-          image="174872318107.dkr.ecr.us-west-2.amazonaws.com/mxnet-algorithms:inference-cpu",
+          image="246618743249.dkr.ecr.us-west-2.amazonaws.com/sagemaker-sklearn-automl:2.5-1-cpu-py3",
           mode="SingleModel",
-          model_data_url="s3://sagemaker-us-west-2-212076617619/mxnet-algorithms/model.tar.gz", 
+          model_data_url="s3://sagemaker-studio-k4u9t44y3hq/auroraml-churn-endpoint/data-processor-models/auroraml-churn-endpoint-dpp0-1-71ca99eb8e894875ada4a2648ab6b555/output/model.tar.gz",
+          environment={
+            "AUTOML_SPARSE_ENCODE_RECORDIO_PROTOBUF":1,
+            "AUTOML_TRANSFORM_MODE":"feature-transform",
+            "SAGEMAKER_DEFAULT_INVOCATIONS_ACCEPT":"application/x-recordio-protobuf",
+            "SAGEMAKER_PROGRAM":"sagemaker_serve",
+            "SAGEMAKER_SUBMIT_DIRECTORY":"/opt/ml/model/code"
+          }
+        ),
+        sagemaker.CfnModel.ContainerDefinitionProperty(
+          image="246618743249.dkr.ecr.us-west-2.amazonaws.com/sagemaker-xgboost:1.3-1-cpu-py3",
+          mode="SingleModel",
+          model_data_url="s3://sagemaker-studio-k4u9t44y3hq/auroraml-churn-endpoint/tuning/auroraml-c-dpp0-xgb/auroraml-churn-endpointpjftIEpiX-237-a8ba5390/output/model.tar.gz",
           environment={
             "MAX_CONTENT_LENGTH":20971520,
-            "ML_APPLICATION":"mlp",
             "SAGEMAKER_DEFAULT_INVOCATIONS_ACCEPT":"text/csv",
             "SAGEMAKER_INFERENCE_OUTPUT":"predicted_label",
             "SAGEMAKER_INFERENCE_SUPPORTED":"predicted_label,probability,probabilities"
@@ -34,7 +44,7 @@ class SMStack(Stack):
         sagemaker.CfnModel.ContainerDefinitionProperty(
           image="246618743249.dkr.ecr.us-west-2.amazonaws.com/sagemaker-sklearn-automl:2.5-1-cpu-py3",
           mode="SingleModel",
-          model_data_url="s3://sagemaker-us-west-2-212076617619/sagemaker-sklearn-automl/model.tar.gz",
+          model_data_url="s3://sagemaker-studio-k4u9t44y3hq/auroraml-churn-endpoint/data-processor-models/auroraml-churn-endpoint-dpp0-1-71ca99eb8e894875ada4a2648ab6b555/output/model.tar.gz",
           environment={
             "AUTOML_TRANSFORM_MODE":"inverse-label-transform",
             "SAGEMAKER_DEFAULT_INVOCATIONS_ACCEPT":"text/csv",
@@ -46,38 +56,26 @@ class SMStack(Stack):
           }
         ),
       ],
-      primary_container=sagemaker.CfnModel.ContainerDefinitionProperty(
-        image="246618743249.dkr.ecr.us-west-2.amazonaws.com/sagemaker-sklearn-automl:2.5-1-cpu-py3",
-        mode="SingleModel",
-        model_data_url="s3://sagemaker-us-west-2-212076617619/model.tar.gz",
-        environment={
-          "AUTOML_SPARSE_ENCODE_RECORDIO_PROTOBUF":1,
-          "AUTOML_TRANSFORM_MODE":"feature-transform",
-          "SAGEMAKER_DEFAULT_INVOCATIONS_ACCEPT":"application/x-recordio-protobuf",
-          "SAGEMAKER_PROGRAM":"sagemaker_serve",
-          "SAGEMAKER_SUBMIT_DIRECTORY":"/opt/ml/model/code"
-        }
-      ),
     )
-#    endpoint_config = sagemaker.CfnEndpointConfig(
-#      self,
-#      id="aurora-ml-endpoint-config",
-#      production_variants=[
-#        sagemaker.CfnEndpointConfig.ProductionVariantProperty(
-#          initial_instance_count=1,
-#          initial_variant_weight=1.0,
-#          instance_type="ml.m5.xlarge",
-#          model_name="aurora-ml",
-#          variant_name="main",
-#        )
-#      ],
-#    )
-#    endpoint_config.add_depends_on(model)
-#    endpoint = sagemaker.CfnEndpoint(
-#      self,
-#      id= "aurora-ml-endpoint",
-#      endpoint_config_name=endpoint_config.attr_endpoint_config_name
-#    )
+    endpoint_config = sagemaker.CfnEndpointConfig(
+      self,
+      id="aurora-ml-endpoint-config",
+      production_variants=[
+        sagemaker.CfnEndpointConfig.ProductionVariantProperty(
+          initial_instance_count=1,
+          initial_variant_weight=1.0,
+          instance_type="ml.m5.xlarge",
+          model_name="aurora-ml",
+          variant_name="main",
+        )
+      ],
+    )
+    endpoint_config.add_depends_on(model)
+    endpoint = sagemaker.CfnEndpoint(
+      self,
+      id= "aurora-ml-endpoint",
+      endpoint_config_name=endpoint_config.attr_endpoint_config_name
+    )
 
 app = App()
 SMStack(app, "aurora-ml")
