8a092c013a829ee42e41dd144d30481d51a3adf4
Biran <birayaha@3c063041fedb.ant.amazon.com>
2022-04-20 13:59:21 -0700

    b_23281

--- build-scripts/.venv/lib/python3.9/site-packages/smdebug_rulesconfig/profiler_rules/utils.py
+++
@@ -1,6 +0,0 @@
-def validate_positive_integer(key, val):
-    assert val > 0, f"{key} must be a positive integer!"
-
-
-def validate_percentile(key, val):
-    assert 0 <= val <= 100, f"{key} must be a valid percentile!"
