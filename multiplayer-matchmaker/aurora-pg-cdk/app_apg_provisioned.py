eb8dd7fd2cd7d2a7353da595ad7b94007c19e08a
Biran <birayaha@3c063041fedb.ant.amazon.com>
2023-04-14 09:23:01 -0700

    remove publicly_accessible=True

--- multiplayer-matchmaker/aurora-pg-cdk/app_apg_provisioned.py
+++ multiplayer-matchmaker/aurora-pg-cdk/app_apg_provisioned.py
@@ -7,28 +7,25 @@ from aws_cdk import (
     App,RemovalPolicy,Stack,Environment
 )
 
-from cdk_nag import ( AwsSolutionsChecks, NagSuppressions )
 
 
 class RDSStack(Stack):
   def __init__(self, app: App, id: str, **kwargs) -> None:
     super().__init__(app, id, **kwargs)
 
-    vpc = ec2.Vpc(self, "VPC")
-    #vpc = ec2.Vpc.from_lookup(self, "VPC",vpc_id="vpc-0bf980360a4cf0521")
+    #vpc = ec2.Vpc(self, "VPC")
+    vpc = ec2.Vpc.from_lookup(self, "VPC",vpc_id="vpc-02bda6f8b28d8359f")
 
-    cluster = rds.DatabaseCluster(self, "match",
+    cluster = rds.DatabaseCluster(self, "rapidscale",
       engine=rds.DatabaseClusterEngine.aurora_postgres(version=rds.AuroraPostgresEngineVersion.VER_13_6),
       credentials=rds.Credentials.from_generated_secret("postgres"),  # Optional - will default to 'admin' username and generated password
       instance_props=rds.InstanceProps(
         instance_type=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.LARGE),
         #instance_type=ec2.InstanceType.of(ec2.InstanceClass.MEMORY6_GRAVITON,ec2.InstanceSize.LARGE),
         vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
-        vpc=vpc,
-        publicly_accessible=True
+        vpc=vpc
       )
     )
-    cluster.connections.allow_from_any_ipv4(ec2.Port.all_traffic(), "Open to the world")
 
 app = App()
 #RDSStack(app, "aurora-ml-pg")
@@ -40,5 +37,5 @@ env = Environment(
         "CDK_DEPLOY_REGION", os.environ.get("CDK_DEFAULT_REGION")
     ),
 )
-RDSStack(app, "stk",env=env)
+RDSStack(app, "rapidscale",env=env)
 app.synth()
